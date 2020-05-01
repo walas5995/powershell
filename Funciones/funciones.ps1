90ea29f8ef222a1339959d16be261beb016e1e57
Rubén Valeiro <walas5995@hotmail.com>
2020-05-01 21:20:23 +0200

    Añadimos un MODULO

---
+++ Funciones/funciones.ps1
@@ -0,0 +1,49 @@
+function connection{
+    <#
+    .SYNOPSYS
+    Descripción de la función
+    .DESCRIPTION
+    Esta es la descripción
+    .EXAMPLE
+    Primer ejemplo 1
+    .EXAMPLE
+    Segundo ejemplo 2
+    #>
+    
+    #Prueba de Github
+    
+    Param(
+    [ValidateSet('deny','permit')]
+    [Parameter(Mandatory=$True,HelpMessage="¿Quieres permitir el acceso remoto a este equipo? 'deny' o 'permit'",Position=1)]
+    [string]$status
+    )
+    
+    if ($status -eq "deny"){
+    #Activar o Desactivar el Firewalle de Windows
+    set-netfirewallprofile -Profile Private -Enabled true
+    #Habilitar Terminal Server en el registro de Windows
+    Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name 'fDenyTSConnections' -value '1'
+    Write-host "****** Firewall - Activado *** Terminal Server - Desactivado ******"
+    }else{
+        if($status -eq "permit"){
+        set-netfirewallprofile -Profile Private -Enabled false
+        Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name 'fDenyTSConnections' -value '0'
+        Write-host "****** Firewall - Desactivado *** Terminal Server - Activado ******"
+        }else{
+        Write-host "****** No se realizó ningun cambio ******"
+        Write-host "******* Utilice 'deny' o 'permit' *******"
+        }
+        }
+    }
+    # ****************** Funciona - Iniciar Sesión remota ******************+
+    function remote{
+        param (
+            [Parameter(Mandatory=$True,Position=1)]
+            [string]$servidor,
+            [Parameter(Mandatory=$True,Position=2)]
+            [string]$usuario
+        )
+        Set-Item WSMan:\localhost\Client\TrustedHosts -Value *
+        enter-pssession -ComputerName $servidor -Credential $usuario
+    }
+    
