b15d054ce5efea16c1dc672b0a9a0c014f3c7498
Rubén Valeiro <walas5995@hotmail.com>
2020-05-03 20:29:50 +0200

    Creamos  la función "CrearHTML"

--- Modulos/ruben.psm1
+++ Modulos/ruben.psm1
@@ -12,10 +12,11 @@ Indice de funciones:
 6-CarpetaBBD
 7-BackupBD
 8-InfoSRV
+9-CrearHTML
 #>
 
 #Función habilitar el escritorio remoto
-function connection{
+function connection {
     
     <#
 
@@ -36,30 +37,32 @@ function connection{
     #Prueba de Github
     
     Param(
-    [ValidateSet('deny','permit')]
-    [Parameter(Mandatory=$True,HelpMessage="¿Quieres permitir el acceso remoto a este equipo? 'deny' o 'permit'",Position=1)]
-    [string]$status
+        [ValidateSet('deny', 'permit')]
+        [Parameter(Mandatory = $True, HelpMessage = "¿Quieres permitir el acceso remoto a este equipo? 'deny' o 'permit'", Position = 1)]
+        [string]$status
     )
     
-    if ($status -eq "deny"){
-    #Activar o Desactivar el Firewalle de Windows
-    set-netfirewallprofile -Profile Private -Enabled true
-    #Habilitar Terminal Server en el registro de Windows
-    Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name 'fDenyTSConnections' -value '1'
-    Write-host "****** Firewall - Activado *** Terminal Server - Desactivado ******"
-    }else{
-        if($status -eq "permit"){
-        set-netfirewallprofile -Profile Private -Enabled false
-        Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name 'fDenyTSConnections' -value '0'
-        Write-host "****** Firewall - Desactivado *** Terminal Server - Activado ******"
-        }else{
-        Write-host "****** No se realizó ningun cambio ******"
-        Write-host "******* Utilice 'deny' o 'permit' *******"
+    if ($status -eq "deny") {
+        #Activar o Desactivar el Firewalle de Windows
+        set-netfirewallprofile -Profile Private -Enabled true
+        #Habilitar Terminal Server en el registro de Windows
+        Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name 'fDenyTSConnections' -value '1'
+        Write-host "****** Firewall - Activado *** Terminal Server - Desactivado ******"
+    }
+    else {
+        if ($status -eq "permit") {
+            set-netfirewallprofile -Profile Private -Enabled false
+            Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name 'fDenyTSConnections' -value '0'
+            Write-host "****** Firewall - Desactivado *** Terminal Server - Activado ******"
         }
+        else {
+            Write-host "****** No se realizó ningun cambio ******"
+            Write-host "******* Utilice 'deny' o 'permit' *******"
         }
+    }
 }
 #Función para crear una conexión remota de powershell
-function remote{
+function remote {
     <#
     .SYNOPSIS
     Función para abrir una conexión "PSSession" remota
@@ -72,14 +75,14 @@ function remote{
         Author:         Rubén Valeiro
         Creation Date:  02-05-2020
     #>
-        param (
-            [Parameter(Mandatory=$True,Position=1)]
-            [string]$servidor,
-            [Parameter(Mandatory=$True,Position=2)]
-            [string]$usuario
-        )
-        Set-Item WSMan:\localhost\Client\TrustedHosts -Value *
-        enter-pssession -ComputerName $servidor -Credential $usuario
+    param (
+        [Parameter(Mandatory = $True, Position = 1)]
+        [string]$servidor,
+        [Parameter(Mandatory = $True, Position = 2)]
+        [string]$usuario
+    )
+    Set-Item WSMan:\localhost\Client\TrustedHosts -Value *
+    enter-pssession -ComputerName $servidor -Credential $usuario
 }
 #Función para obtener la contraseña para enviar el correo
 function ObtenerPwdMail () {
@@ -95,15 +98,17 @@ function ObtenerPwdMail () {
     #>
     $carpetaPWD = Get-Item C:\scripts
     $archicoPWD = Get-ChildItem -Path "C:\scripts\password_correo.txt"
-    if($archicoPWD){
+    if ($archicoPWD) {
         $password = Get-Content "c:\scripts\password_correo.txt" | ConvertTo-SecureString 
     
-    }else{
-        if($carpetaPWD){
+    }
+    else {
+        if ($carpetaPWD) {
             (Get-Credential).Password | ConvertFrom-SecureString | Out-File C:\scripts\password_correo.txt
             $password = Get-Content "c:\scripts\password_correo.txt" | ConvertTo-SecureString 
     
-        }else{
+        }
+        else {
             mkdir C:\scripts
             (Get-Credential).Password | ConvertFrom-SecureString | Out-File C:\scripts\password_correo.txt
             $password = Get-Content "c:\scripts\password_correo.txt" | ConvertTo-SecureString 
@@ -126,15 +131,17 @@ function ObtenerPwdBD () {
     #>
     $carpetaPWD = Get-Item C:\scripts
     $archicoPWD = Get-ChildItem -Path "C:\scripts\password_BD.txt"
-    if($archicoPWD){
+    if ($archicoPWD) {
         $password = Get-Content "c:\scripts\password_BD.txt" | ConvertTo-SecureString 
     
-    }else{
-        if($carpetaPWD){
+    }
+    else {
+        if ($carpetaPWD) {
             (Get-Credential).Password | ConvertFrom-SecureString | Out-File C:\scripts\password_BD.txt
             $password = Get-Content "c:\scripts\password_BD.txt" | ConvertTo-SecureString 
     
-        }else{
+        }
+        else {
             mkdir C:\scripts
             (Get-Credential).Password | ConvertFrom-SecureString | Out-File C:\scripts\password_BD.txt
             $password = Get-Content "c:\scripts\password_BD.txt" | ConvertTo-SecureString 
@@ -161,18 +168,18 @@ function enviar_mail () {
         Creation Date:  02-05-2020
     #>
     param (
-        [Parameter(Mandatory=$True,Position=1)]
+        [Parameter(Mandatory = $True, Position = 1)]
         [string]$asunto,
-        [Parameter(Mandatory=$True,Position=2)]
+        [Parameter(Mandatory = $True, Position = 2)]
         [string]$descrip
     )
     $password = ObtenerPwdMail
 
-    $rmail= "walas5995@gmail.com" 
-    $email="incidenciasvsm@gmail.com"
-    $servermail="smtp.gmail.com"
-    $puerto="587"
-    $cred = New-Object System.Management.Automation.PsCredential("incidenciasvsm@gmail.com",$password)
+    $rmail = "walas5995@gmail.com" 
+    $email = "incidenciasvsm@gmail.com"
+    $servermail = "smtp.gmail.com"
+    $puerto = "587"
+    $cred = New-Object System.Management.Automation.PsCredential("incidenciasvsm@gmail.com", $password)
     Send-MailMessage -To $rmail -Subject $asunto -body $descrip -from $email -SmtpServer $servermail -Port $puerto -Credential $cred -usessl       
 }
 #Función que comprueba si existe la carpeta donde guardar la copia
@@ -188,15 +195,16 @@ function carpetaBBD () {
         Creation Date:  02-05-2020
     #>
     $carpetaPWD = Get-Item C:\backupBD
-        if($carpetaPWD){
+    if ($carpetaPWD) {
         #la carpeta existe !!!
-        }else{
-            mkdir C:\backupBD
-        }
+    }
+    else {
+        mkdir C:\backupBD
+    }
 }
 #Función que guarda una copia de seguridad de la BD que le indiquemos
-function backupBD(){
-        <#
+function backupBD() {
+    <#
         .SYNOPSIS
         Backup de la base de datos.
         .DESCRIPTION
@@ -208,29 +216,28 @@ function backupBD(){
         Author:         Rubén Valeiro
         Creation Date:  02-05-2020
         #>
-        param (
-            [Parameter(Mandatory=$True,Position=1)]
-            [string]$instanceDB,
-            [Parameter(Mandatory=$True,Position=2)]
-            [string]$nameDB
-        )
-        $pwd = ObtenerPwdBD
-        carpetaBBD
-        $cred = New-Object System.Management.Automation.PSCredential("sa",$pwd)
-        #Ejecutar Consulta
-        #Invoke-Sqlcmd -ServerInstance ".\sqlexpress" -Query "select * from BD_Test.dbo.personal"
+    param (
+        [Parameter(Mandatory = $True, Position = 1)]
+        [string]$instanceDB,
+        [Parameter(Mandatory = $True, Position = 2)]
+        [string]$nameDB
+    )
+    $pwd = ObtenerPwdBD
+    carpetaBBD
+    $cred = New-Object System.Management.Automation.PSCredential("sa", $pwd)
+    #Ejecutar Consulta
+    #Invoke-Sqlcmd -ServerInstance ".\sqlexpress" -Query "select * from BD_Test.dbo.personal"
         
-        #backup de la base de datos
-        #InstanceDB (default) --> ".\sqlexpress"
-        $carpeta = "c:\backupBD"
-        $fecha = Get-Date -UFormat "%Y%m%d"
-        $archivo= "c:\backupBD\"+$nameDB+"_"+$fecha+".bak"
+    #backup de la base de datos
+    #InstanceDB (default) --> ".\sqlexpress"
+    $fecha = Get-Date -UFormat "%Y%m%d"
+    $archivo = "c:\backupBD\" + $nameDB + "_" + $fecha + ".bak"
 
-        Backup-SqlDatabase -BackupFile $archivo -ServerInstance $instanceDB -Database $nameDB -Credential $cred
+    Backup-SqlDatabase -BackupFile $archivo -ServerInstance $instanceDB -Database $nameDB -Credential $cred
 
 
 }
-function infoSRV{
+function infoSRV {
     <#
             .SYNOPSIS
             Mostrar información de los servidores de la empresa
@@ -243,26 +250,26 @@ function infoSRV{
             Author:         Rubén Valeiro
             Creation Date:  03-05-2020
             #>
-            param (
-                [Parameter(Mandatory=$True,Position=1)]
-                [string]$srv
-            )
-            $infoPC = Get-ComputerInfo
-            Write-Host "******* INFO *******"
-            Write-Host "       "
-            Write-Host "Nombre: " $infoPC.CsName
-            Write-Host "Dominio: " $infoPC.CsDomain
-            Write-Host "Sistema Operativo: " $infoPC.WindowsProductName
-            Write-Host "Arquitectura: " $infoPC.OsArchitecture
-            #Write-Host "Procesador: " $infoPC.CsProcessors
-            #Memoria
-            Write-Host "       "
-            Write-Host "Memoria RAM"
-            Write-Host "       "
-            $PysicalMemory = Get-WmiObject -class "win32_physicalmemory" -namespace "root\CIMV2" -ComputerName $srv
-            write-Host "Total: $((($PysicalMemory).Capacity | Measure-Object -Sum).Sum/1GB)GB" 
-            #Disco Duro
-            <#
+    param (
+        [Parameter(Mandatory = $True, Position = 1)]
+        [string]$srv
+    )
+    $infoPC = Get-ComputerInfo
+    Write-Host "******* INFO *******"
+    Write-Host "       "
+    Write-Host "Nombre: " $infoPC.CsName
+    Write-Host "Dominio: " $infoPC.CsDomain
+    Write-Host "Sistema Operativo: " $infoPC.WindowsProductName
+    Write-Host "Arquitectura: " $infoPC.OsArchitecture
+    #Write-Host "Procesador: " $infoPC.CsProcessors
+    #Memoria
+    Write-Host "       "
+    Write-Host "Memoria RAM"
+    Write-Host "       "
+    $PysicalMemory = Get-WmiObject -class "win32_physicalmemory" -namespace "root\CIMV2" -ComputerName $srv
+    write-Host "Total: $((($PysicalMemory).Capacity | Measure-Object -Sum).Sum/1GB)GB" 
+    #Disco Duro
+    <#
             $disco_libre = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | select -ExpandProperty freespace
             $disco_total = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" |
             select -ExpandProperty size
@@ -270,44 +277,54 @@ function infoSRV{
             Write-Host "Libre: "$disco_libre
             Write-Host "Total: "$disco_total
             #>
-            #Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" |
-            #Measure-Object -Property FreeSpace,Size -Sum |
-            #Select-Object -Property Property,Sum           
-            #Disco Duro
-            Write-Host "       "
-            Write-Host "Disco Duro"
-            Write-Host "       "
-            $partitions= Get-WmiObject -Class Win32_LogicalDisk -Filter 'DriveType = 3' |Select-Object PSComputerName, Caption,@{N='Capacity_GB'; E={[math]::Round(($_.Size / 1GB), 2)}},@{N='FreeSpace_GB'; E={[math]::Round(($_.FreeSpace / 1GB), 2)}},@{N='PercentUsed'; E={[math]::Round(((($_.Size - $_.FreeSpace) / $_.Size) * 100), 2) }},@{N='PercentFree'; E={[math]::Round((($_.FreeSpace / $_.Size) * 100), 2) }}
-            foreach($z in $partitions)
-            {  
-                Write-Host "Particion         : $($z.Caption)" ;    
-                Write-Host "Capacidad Total   : $($z.Capacity_GB)  GB" ;    
-                Write-Host "Espacio Libre     : $($z.FreeSpace_GB) GB" ;    
-                Write-Host "Porcentaje Usado  : $($z.PercentUsed)  %" ;    
-                Write-Host "Porcentaje Libre  : $($z.PercentFree)  %" ;   
-                Write-Host "";   
-            }
-            Write-Host "       "
-            <#
-            $disco_libre = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | select -ExpandProperty freespace
-            $disco_total = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" |
-            select -ExpandProperty size
-            Write-Host "Disco Duro - Personal"
-            Write-Host "Libre: "$disco_libre
-            Write-Host "Total: "$disco_total
-            #>
-            #Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" |
-            #Measure-Object -Property FreeSpace,Size -Sum |
-            #Select-Object -Property Property,Sum
-            #Red 
-            $ips=Get-NetIPAddress | where {$_.AddressFamily -eq "IPv4"}
-            Write-Host "Direcciones IP"
-            Write-Host "       "
-            foreach($ip in $ips){
-                Write-Host "*: "$ip.ipaddress
-            }
-            Write-Host "       "            
+    #Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" |
+    #Measure-Object -Property FreeSpace,Size -Sum |
+    #Select-Object -Property Property,Sum           
+    #Disco Duro
+    Write-Host "       "
+    Write-Host "Disco Duro"
+    Write-Host "       "
+    $partitions = Get-WmiObject -Class Win32_LogicalDisk -Filter 'DriveType = 3' | Select-Object PSComputerName, Caption, @{N = 'Capacity_GB'; E = { [math]::Round(($_.Size / 1GB), 2) } }, @{N = 'FreeSpace_GB'; E = { [math]::Round(($_.FreeSpace / 1GB), 2) } }, @{N = 'PercentUsed'; E = { [math]::Round(((($_.Size - $_.FreeSpace) / $_.Size) * 100), 2) } }, @{N = 'PercentFree'; E = { [math]::Round((($_.FreeSpace / $_.Size) * 100), 2) } }
+    foreach ($z in $partitions) {  
+        Write-Host "Particion         : $($z.Caption)" ;    
+        Write-Host "Capacidad Total   : $($z.Capacity_GB)  GB" ;    
+        Write-Host "Espacio Libre     : $($z.FreeSpace_GB) GB" ;    
+        Write-Host "Porcentaje Usado  : $($z.PercentUsed)  %" ;    
+        Write-Host "Porcentaje Libre  : $($z.PercentFree)  %" ;   
+        Write-Host "";   
+    }
+    Write-Host "       "
+    #Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" |
+    #Measure-Object -Property FreeSpace,Size -Sum |
+    #Select-Object -Property Property,Sum
+    #Red 
+    $ips = Get-NetIPAddress | Where-Object { $_.AddressFamily -eq "IPv4" }
+    Write-Host "Direcciones IP"
+    Write-Host "       "
+    foreach ($ip in $ips) {
+        Write-Host "*: "$ip.ipaddress
+    }
+    Write-Host "       "            
 }
-
-   
-    
+function crearHTML{
+    <#
+            .SYNOPSIS
+            Crear informe en HTML
+            .DESCRIPTION
+            Crear informe en HTML
+            .EXAMPLE
+            crearHTML "variable"
+            .NOTES
+            Version:        1.0
+            Author:         Rubén Valeiro
+            Creation Date:  03-05-2020
+            #>
+            param (
+                [Parameter(Mandatory = $True, Position = 1)]
+                $info
+            )
+            write-Host $info | select csname
+            #$resultado = $info | select csname,csdomain,WindowsProductName,OsArchitecture | ConvertTo-EnhancedHTMLFragment -As Table -PreContent "<h2> Normal Services</h2>"
+            # Opción   -CssUri C:\scripts\style.css
+            #ConvertTo-EnhancedHTML -HTMLFragments $resultado | Out-File c:\scripts\informe.html
+}
