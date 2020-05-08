f7f258e9b9aa9ae80dd12f78c418d6751de807ff
Rubén Valeiro <walas5995@hotmail.com>
2020-05-08 19:58:31 +0200

    Pruebas con las funciones para el AD

--- Funciones/ActiveDirectory.ps1
+++ Funciones/ActiveDirectory.ps1
@@ -6,6 +6,13 @@ nltest /dcname:$dominio
 #Obternet nombre e IP del servidor que tiene el ROL indicado
 nltest /dsgetdc:$dominio /PDC
 #/PDC /DS /DSP /GC /KDC /TIMESERV /GTIMESERV /WS /NETBIOS /DNS /IP /FORCE /WRITABLE /AVOIDSELF /LDAPONLY /BACKG /DS_6 /DS_8 /DS_9 /DS_10
-
 #Información del controlador de dominio consultado
 Get-ADDomainController -Server ws2019
+#Obtener nombre del servidor que tiene el Rol especificado
+#Valores admitidos:PrimaryDC, GlobalCatalog, KDC, TimeService, ReliableTimeService, ADWS"
+Get-ADDomainController -discover -Service "PrimaryDC" | select -ExpandProperty name
+Get-ADDomainController -discover -Service "GlobalCatalog" | select -ExpandProperty name
+Get-ADDomainController -discover -Service "KDC" | select -ExpandProperty name
+Get-ADDomainController -discover -Service "TimeService" | select -ExpandProperty name
+Get-ADDomainController -discover -Service "ADWS" | select -ExpandProperty name
+Get-ADDomainController -discover -Service "ReliableTimeService" | select -ExpandProperty name
